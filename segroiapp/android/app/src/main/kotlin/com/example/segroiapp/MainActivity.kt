214bd6c7d27c26b69bbe0c80f42dcbc48acb1dbf
ziuwuchuoi <vv0320nt@gmail.com>
2024-06-10 02:48:30 +0700

    build

---
+++ segroiapp/android/app/src/main/kotlin/com/example/segroiapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.segroiapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ segroiapp/lib/config/themes/app_text_styles.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xFFFF9AA2);
+  static const secondaryColor = Color(0xFFC7CEEA);
+  static const lightPink = Color(0xFFFFB7B2);
+  static const lightOrange = Color(0xFFFFDAC1);
+  static const lightGreen = Color(0xFFE2F0CB);
+  static const lightGray = Color(0xFFD5D5D5);
+  static const mint = Color(0xFFB5EAD7);
+  static const fontColor = Color(0xFF404040);
+  static const fade = Color(0xFFD5D5D5);
+  static const lineColor = Color(0xFFE0E0E0);
+  static const lightperiwinkle = Color(0xFFC7CEE3);
+  static const redPigment = Color(0xFFF12424);
+  static const isabelline = Color(0xffEDFAFA);
+  static const blue = Color(0xFF6880FD);
+  static const white = Color(0xFFFFFFFF);
+  static const inputValue = Color(0xFF070C1E);
+  static const pinkRed = Color(0xFFFF9AA2);
+  static const startDust = Color(0xFF979C9E);
+  static const jordyBlue = Color(0xFF92A8F6);
+  static const lightBlue = Color(0xFF92A8F6);
+  static const blueText = Color(0xFF6880FD);
+  static const blueDarkText = Color(0xFF051C40);
+  static const grey = Color(0xFFE6E6E6);
+  static const black = Color(0x00000000);
+  static const textBlack = Color(0xFF404040);
+  static const yellow = Color(0xFFFCFF7B);
+  static const groupBlack = Color(0xFF070C1E);
+}
